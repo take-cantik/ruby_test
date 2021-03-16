724f83f07b05dd17b527485b1073a6ff62da41fa
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-03-16 20:59:53 +0900

    Create ruby template

---
+++ lib/ruby_gihub_actions_test.rb
@@ -0,0 +1,6 @@
+require "ruby_gihub_actions_test/version"
+
+module RubyGihubActionsTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ruby_gihub_actions_test/version.rb
@@ -0,0 +1,3 @@
+module RubyGihubActionsTest
+  VERSION = "0.1.0"
+end
